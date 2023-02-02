91f2cb5a0ee045cddbf7a1e2174eb3b8b937f2f1
Tainara-SM <tainarasm1@hotmail.com>
2023-02-02 12:44:10 -0300

    Initial commit

---
+++ Exercicios/ex007/4.js
@@ -0,0 +1,6 @@
+console.log("This is the first line\nAnd this is the second")
+console.log("A newline character is written like \"\\n\".")
+console.log("exemplo seria \\obs")
+console.log(3 > 2)
+console.log("a" > "b")
+console.log(Math.min(2, 4) + 100);
